3ff45bc361478944104ba7ae34e7dce94ad7507b
Hakim Hauston <hakim.hauston@singtel.com>
2020-02-25 10:33:48 +0800

    [amplify] ui: sign in - working without the amplify project

--- lerna/packages/rnamplify2/App.js
+++ lerna/packages/rnamplify2/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import Amplify from 'aws-amplify';
 import { Auth } from 'aws-amplify';
-import { Button, Text } from 'react-native'
+import { Button, Text, View } from 'react-native'
 import awsconfig from './aws-exports';
 
 Amplify.configure(awsconfig);
 
 export default Scene = (props) => {
+  const onPressSignIn = async () => {
+    const user = await Auth.signIn('username4@email.com', 'password');
+    console.log(user);
+  }
   return (
+    <View>
     <Button title="SIGN UP" onPress={() => {
       console.log('btn pressed');
-      Auth.signUp({username: 'username2@email.com', password: 'password'}).then(data => {
+      Auth.signUp({username: 'username4@email.com', password: 'password'}).then(data => {
         console.log('signUp then');
         console.log(data);
       }).catch(err => {
@@ -18,5 +23,7 @@ export default Scene = (props) => {
         console.log(err);
       });
     }}/>
+    <Button title="SIGN IN" onPress={onPressSignIn}/>
+    </View>
   );
 }
